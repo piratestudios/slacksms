6e3989916c893e321d7dfbb117147bf9a6bfe467
Pete Black <pete@piratestudios.co.uk>
2019-11-22 16:17:01 +0000

    Strip body text of <tel:+447xxxxxxxxx|+447xxxxxxxxx> nonsense

--- smsbot/twilio.rb
+++ smsbot/twilio.rb
@@ -16,6 +16,9 @@ module SMSBot
         # <tel:+447xxxxxxxxx|+447xxxxxxxxx> so use a regex to capture
         # the first set of numbers.
         to = to[/(\d+)/]
+        # Do the same for the body, stripping as many <tel:+1234|+1234>
+        # that occur.
+        body = body.gsub(/<tel:(\+\d+)\|\+\d+>/, '\1')
 
         client.messages.create(
           messaging_service_sid: messaging_service_sid,
